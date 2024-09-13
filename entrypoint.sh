74386c17b67fc3b9c85a61aa210b977226cf9645
Edwin van Buuren <edwin.vanbuuren@ioki.com>
2024-09-13 14:49:45 +0200

    fully working except ddl tracker

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,12 @@ if [[ $UID -ge 10000 ]]; then
     rm /tmp/passwd
 fi
 
-# Create the data directory if it doesn't exist
+# Ensure the data directory has the correct permissions
 if [ ! -d "/home/postgres/pgdata/pgroot/data" ]; then
     mkdir -p /home/postgres/pgdata/pgroot/data
-    chown -R postgres:postgres /home/postgres/pgdata
 fi
+chown -R postgres:postgres /home/postgres/pgdata/pgroot/data
+chmod 0700 /home/postgres/pgdata/pgroot/data
 
 cat > /home/postgres/patroni.yml <<__EOF__
 bootstrap:
@@ -21,6 +22,10 @@ bootstrap:
       pg_hba:
       - host all all 0.0.0.0/0 md5
       - host replication ${PATRONI_REPLICATION_USERNAME} ${PATRONI_KUBERNETES_POD_IP}/16 md5
+      - host replication ${PATRONI_REPLICATION_USERNAME} ${PATRONI_KUBERNETES_POD_IP}/16 md5
+      - local all all trust  # Allow local connections for all users
+      - host replication ${PATRONI_REPLICATION_USERNAME} 127.0.0.1/32 trust  # Allow localhost for replication
+      - host replication ${PATRONI_REPLICATION_USERNAME} ::1/128 trust  # IPv6 localhost
   initdb:
   - auth-host: md5
   - auth-local: trust
@@ -31,12 +36,12 @@ restapi:
   connect_address: '${PATRONI_KUBERNETES_POD_IP}:8008'
 postgresql:
   connect_address: '${PATRONI_KUBERNETES_POD_IP}:5432'
+  data_dir: '${PATRONI_POSTGRESQL_DATA_DIR}'  # Set the data_dir from the environment variable
   authentication:
     superuser:
       password: '${PATRONI_SUPERUSER_PASSWORD}'
     replication:
       password: '${PATRONI_REPLICATION_PASSWORD}'
-  data_dir: '${PATRONI_POSTGRESQL_DATA_DIR}'  # Added data_dir here
 kubernetes:
   namespace: '${PATRONI_KUBERNETES_NAMESPACE}'
   labels:
@@ -44,7 +49,7 @@ kubernetes:
     cluster-name: patroni-postgres
   role_label: role
   pod_ip: ${PATRONI_KUBERNETES_POD_IP}
-  name: ${PATRONI_NAME}
+  name: ${PATRONI_NAME}  # Ensure the pod name is passed correctly
 __EOF__
 
 unset PATRONI_SUPERUSER_PASSWORD PATRONI_REPLICATION_PASSWORD
